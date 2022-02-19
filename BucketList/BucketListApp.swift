02ca3db0db73c30faddb544dca48efad24eaa128
citixenken <kmuyesu@gmail.com>
2022-02-19 22:50:54 +0300

    first commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Ken Muyesu on 19/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
