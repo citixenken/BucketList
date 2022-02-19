4afb49686b570c446d95261607fa4094a9a74a12
citixenken <kmuyesu@gmail.com>
2022-02-20 00:34:00 +0300

    writing data to the Documents Directory

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    func getDocumentsDirectory() -> URL {
+        //find all possible documents directories for this user
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        //just send back the first one, ought to be the only one
+        return paths[0]
+    }
+    
     var body: some View {
         Text("Hello, world!")
+            .onTapGesture {
+                let str = "Test Message"
+                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+                
+                do {
+                    try str.write(to: url, atomically: true, encoding: .utf8)
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
             .padding()
     }
 }
