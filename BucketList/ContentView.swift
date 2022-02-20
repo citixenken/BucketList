fde18fa66f69e20cbd0eeeb858c48d07aef240e8
citixenken <kmuyesu@gmail.com>
2022-02-20 13:14:49 +0300

    using biometrics with SwiftUI

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,63 +7,100 @@
 
 import SwiftUI
 import MapKit
+import LocalAuthentication
 
 struct ContentView: View {
     
-//    func getDocumentsDirectory() -> URL {
-//        //find all possible documents directories for this user
-//        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-//
-//        //just send back the first one, ought to be the only one
-//        return paths[0]
-//    }
+    //    func getDocumentsDirectory() -> URL {
+    //        //find all possible documents directories for this user
+    //        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    //
+    //        //just send back the first one, ought to be the only one
+    //        return paths[0]
+    //    }
     @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    @State private var isUnlocked = false
     
     let locations = [
         Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
         Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
     ]
     var body: some View {
-        
-        NavigationView {
-            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-                MapAnnotation(coordinate: location.coordinate) {
-                    NavigationLink {
-                        Text(location.name)
-                    } label: {
-                        Circle()
-                            .stroke(.red, lineWidth: 2)
-                            .frame(width: 20, height: 20)
-    //                        .onTapGesture {
-    //                            print("Tapped on \(location.name)")
-    //                        }
-                    }
-                    
-                }
-                
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
             }
-            .navigationTitle("London Explorer")
         }
-//        Text("Hello, world!")
-//            .onTapGesture {
-//                let str = "Test Message"
-//                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+        .onAppear(perform: authenticate)
+        
+        
+//        NavigationView {
+//            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+//                MapAnnotation(coordinate: location.coordinate) {
+//                    NavigationLink {
+//                        Text(location.name)
+//                    } label: {
+//                        Circle()
+//                            .stroke(.red, lineWidth: 2)
+//                            .frame(width: 20, height: 20)
+//                        //                        .onTapGesture {
+//                        //                            print("Tapped on \(location.name)")
+//                        //                        }
+//                    }
 //
-//                do {
-//                    try str.write(to: url, atomically: true, encoding: .utf8)
-//                    let input = try String(contentsOf: url)
-//                    print(input)
-//                } catch {
-//                    print(error.localizedDescription)
 //                }
+//
 //            }
-//            .padding()
+//            .navigationTitle("London Explorer")
+//        }
+        
+        
+        //        Text("Hello, world!")
+        //            .onTapGesture {
+        //                let str = "Test Message"
+        //                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+        //
+        //                do {
+        //                    try str.write(to: url, atomically: true, encoding: .utf8)
+        //                    let input = try String(contentsOf: url)
+        //                    print(input)
+        //                } catch {
+        //                    print(error.localizedDescription)
+        //                }
+        //            }
+        //            .padding()
     }
     
     struct Location: Identifiable {
         let id = UUID()
         let name: String
         let coordinate: CLLocationCoordinate2D
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        //check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            //if possible, go ahead and use it
+            let reason = "We need to unlock your data."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                //authentication has now completed
+                if success {
+                    isUnlocked = true
+                } else {
+                        //there was a problem
+                    }
+                
+            }
+        } else {
+            //no biometrics
+        }
     }
 }
 
