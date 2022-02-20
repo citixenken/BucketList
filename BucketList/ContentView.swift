a75ce2cd0e4b74dc333a3868706124ac33cf812f
citixenken <kmuyesu@gmail.com>
2022-02-20 12:50:02 +0300

    integrating MapKit with SwiftUI

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,32 +6,64 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     
-    func getDocumentsDirectory() -> URL {
-        //find all possible documents directories for this user
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+//    func getDocumentsDirectory() -> URL {
+//        //find all possible documents directories for this user
+//        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+//
+//        //just send back the first one, ought to be the only one
+//        return paths[0]
+//    }
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    var body: some View {
         
-        //just send back the first one, ought to be the only one
-        return paths[0]
+        NavigationView {
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                MapAnnotation(coordinate: location.coordinate) {
+                    NavigationLink {
+                        Text(location.name)
+                    } label: {
+                        Circle()
+                            .stroke(.red, lineWidth: 2)
+                            .frame(width: 20, height: 20)
+    //                        .onTapGesture {
+    //                            print("Tapped on \(location.name)")
+    //                        }
+                    }
+                    
+                }
+                
+            }
+            .navigationTitle("London Explorer")
+        }
+//        Text("Hello, world!")
+//            .onTapGesture {
+//                let str = "Test Message"
+//                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+//
+//                do {
+//                    try str.write(to: url, atomically: true, encoding: .utf8)
+//                    let input = try String(contentsOf: url)
+//                    print(input)
+//                } catch {
+//                    print(error.localizedDescription)
+//                }
+//            }
+//            .padding()
     }
     
-    var body: some View {
-        Text("Hello, world!")
-            .onTapGesture {
-                let str = "Test Message"
-                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
-                
-                do {
-                    try str.write(to: url, atomically: true, encoding: .utf8)
-                    let input = try String(contentsOf: url)
-                    print(input)
-                } catch {
-                    print(error.localizedDescription)
-                }
-            }
-            .padding()
+    struct Location: Identifiable {
+        let id = UUID()
+        let name: String
+        let coordinate: CLLocationCoordinate2D
     }
 }
 
